a7447e404d0bbd3754b23a957f95884d4da49838
elisa33 <pipi.tdfuego@gmail.com>
2021-02-14 13:01:38 -0400

    pagination

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const formulario = document.querySelector('#formulario');
 const resultado = document.querySelector('#resultado');
-const paginacion = document.querySelector('#paginacion');
+const paginacionDiv = document.querySelector('#paginacion');
+
+const registroPorPagina = 40;
+let totalPaginas;
+let iterador;
 
 window.onload = () => {
 	formulario.addEventListener('submit', validarFormulario);
@@ -42,12 +46,78 @@ function mostrarAlerta(mensaje) {
 
 function buscarImagenes(termino) {
 	const key = '1362366-2f4384e21f5db730c4eefbb59';
-	const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
-	console.log(url);
+	const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registroPorPagina}`;
 
 	fetch(url)
 		.then((respuesta) => respuesta.json())
 		.then((resultado) => {
+			totalPaginas = calcularPaginas(resultado.totalHits);
 			mostrarImagenes(resultado.hits);
 		});
+}
+// generador
+function* crearPaginador(total) {
+	for (let i = 1; i <= total; i++) {
+		yield i;
+	}
+}
+
+function calcularPaginas(total) {
+	return parseInt(Math.ceil(total / registroPorPagina));
+}
+
+function mostrarImagenes(imagenes) {
+	while (resultado.firstChild) {
+		resultado.removeChild(resultado.firstChild);
+	}
+
+	imagenes.forEach((imagen) => {
+		const { previewURL, likes, views, largeImageURL } = imagen;
+		resultado.innerHTML += `
+        <div class='w-1/2 md:w1/3 lg:w-1/4 p-3 mb-4'>
+            <div class="bg-white">
+              <img class='w-full'src='${previewURL}'>
+            <div class='p-4'>
+				<p class='font-bold'> ${likes}<span class='font-light'> Me gusta </span> </p>
+				<p class='font-bold'> ${views}<span class='font-light'> Veces vista </span> </p>
+				<a class='block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1' href='${largeImageURL}' target='_blank' rel='noopener noreferrer'> Ver imágen </a>
+			</div>
+            </div>
+        
+        </div>
+        `;
+	});
+
+	//limpiar el paginador previo
+	while (paginacionDiv.firstChild) {
+		paginacionDiv.removeChild(paginacionDiv.firstChild);
+	}
+
+	imprimirPaginador();
+}
+
+function imprimirPaginador() {
+	iterador = crearPaginador(totalPaginas);
+
+	console.log('while 104');
+
+	const { value, done } = iterador.next;
+	if (done) return;
+	const boton = document.createElement('a');
+	boton.href = '#';
+	boton.dataset.pagina = value;
+	boton.textContent = value;
+	boton.classList.add(
+		'mb-4',
+		'bg-yellow-400',
+		'px-4',
+		'py-1',
+		'font-bold',
+		'uppercase',
+		'rounded',
+		'flex',
+		'flex-wrap'
+	);
+
+	paginacionDiv.appendChild(boton);
 }
